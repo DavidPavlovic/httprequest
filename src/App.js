8b315fbea3ef76f613229421868e88c4a7cf4d11
David <davidpavlovicdev@gmail.com>
2022-02-15 11:02:29 +0100

    add error

--- src/App.js
+++ src/App.js
@@ -6,20 +6,33 @@ import './App.css';
 function App() {
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     async function fetchMoviesHandler() {
         setIsLoading(true);
-        const response = await fetch('https://swapi.dev/api/films/');
-        const data = await response.json();
-        const transormedMovies = data.results.map(movieData => {
-            return {
-                id: movieData.episode_id,
-                title: movieData.title,
-                openingText: movieData.opening_crawl,
-                releaseDate: movieData.release_date
+        setError(null);
+        try {
+            const response = await fetch('https://swapi.dev/api/films/');
+
+            if(!response.ok) {
+                throw new Error('Something went wrong')
             }
-        });
-        setMovies(transormedMovies);
+
+            const data = await response.json();
+
+            const transormedMovies = data.results.map(movieData => {
+                return {
+                    id: movieData.episode_id,
+                    title: movieData.title,
+                    openingText: movieData.opening_crawl,
+                    releaseDate: movieData.release_date
+                }
+            });
+            setMovies(transormedMovies);
+        } catch(error) {
+            setError(error.message);
+        }
+
         setIsLoading(false);
     }
 
@@ -30,8 +43,9 @@ function App() {
             </section>
             <section>
                 {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-                {!isLoading && movies.length === 0 && <p>Found no movie.</p>}
+                {!isLoading && movies.length === 0 && !error && <p>Found no movie.</p>}
                 {isLoading && <p>Loading...</p>}
+                {!isLoading && error && <p>{error}</p>}
             </section>
         </React.Fragment>
     );
