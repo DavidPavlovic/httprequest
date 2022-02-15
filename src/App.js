a9f5cd80b43dd59da0d6e9fb835d527b836d88c7
David <davidpavlovicdev@gmail.com>
2022-02-15 12:30:04 +0100

    add post method

--- src/App.js
+++ src/App.js
@@ -14,23 +14,25 @@ function App() {
         setIsLoading(true);
         setError(null);
         try {
-            const response = await fetch('https://swapi.dev/api/films/');
+            const response = await fetch('https://reactapp-http-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
 
             if(!response.ok) {
                 throw new Error('Something went wrong')
             }
 
             const data = await response.json();
+            const loadedMovies = [];
 
-            const transormedMovies = data.results.map(movieData => {
-                return {
-                    id: movieData.episode_id,
-                    title: movieData.title,
-                    openingText: movieData.opening_crawl,
-                    releaseDate: movieData.release_date
-                }
-            });
-            setMovies(transormedMovies);
+            for(const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate
+                })
+            }
+            
+            setMovies(loadedMovies);
         } catch(error) {
             setError(error.message);
         }
@@ -38,13 +40,27 @@ function App() {
         setIsLoading(false);
     }, []);
 
-    useEffect(() => {
-        fetchMoviesHandler();
-    }, [fetchMoviesHandler]);
+    const addMovieHandler = useCallback(async (movie) => {
+        setError(null);
+        try {
+            const response = await fetch('https://reactapp-http-default-rtdb.europe-west1.firebasedatabase.app/movies.json', {
+                method: 'POST',
+                body: JSON.stringify(movie),
+                header: {
+                    'Content-tpe': 'application/json'
+                }
+            });
 
-    function addMovieHandler(movie) {
-        console.log(movie);
-    }
+            if(!response.ok) {
+                throw new Error('Something went wrong')
+            }
+
+            const data = response.json();
+            console.log(data)
+        } catch (error) {
+            setError('Something isnt right');
+        }
+    }, []);
 
     if(movies.length > 0) {
         content = <MoviesList movies={movies} />;
@@ -57,6 +73,10 @@ function App() {
     if(isLoading) {
         content = <p>Loading...</p>;
     }
+
+    useEffect(() => {
+        fetchMoviesHandler();
+    }, [fetchMoviesHandler]);
 
     return (
         <React.Fragment>
